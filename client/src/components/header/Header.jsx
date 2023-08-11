6b30c624112280ced802f88518414099e101a5a8
Darshik-15 <dbmarvaniya2002@gmail.com>
2023-08-11 23:23:29 +0530

    Blog_Site

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+import ima from "../../Assets/well-being.webp" 
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src={ima}
+        alt=""
+      />
+    </div>
+  );
+}
